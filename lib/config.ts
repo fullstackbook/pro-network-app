f6d874bd9c544b535ef38875dea179d2831de987
Travis Luong <travis@travisluong.com>
2024-01-18 16:28:47 +0800

    create config

---
+++ lib/config.ts
@@ -0,0 +1,16 @@
+import { loadEnvConfig } from "@next/env";
+
+const projectDir = process.cwd();
+loadEnvConfig(projectDir);
+
+const config = {
+  POSTGRES_URL: process.env.POSTGRES_URL!,
+  NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET!,
+  GITHUB_ID: process.env.GITHUB_ID!,
+  GITHUB_SECRET: process.env.GITHUB_SECRET!,
+  APP_ENV: process.env.APP_ENV!,
+};
+
+// console.log(config)
+
+export default config;
