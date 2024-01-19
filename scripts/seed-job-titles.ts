f8c432d8ce3982f907db616e463e03c1c97af33e
Travis Luong <travis@travisluong.com>
2024-01-19 15:54:50 +0800

    seed job titles

---
+++ scripts/seed-job-titles.ts
@@ -0,0 +1,26 @@
+import { db, pool } from "@/lib/db";
+import { users } from "@/lib/schema";
+import { eq } from "drizzle-orm";
+
+const JOB_TITLES = [
+  "Full Stack Developer",
+  "Back End Developer",
+  "Front End Developer",
+  "Project Manager",
+  "DevOps Engineer",
+  "Data Scientist",
+];
+
+async function main() {
+  const res = await db.query.users.findMany();
+  for (let user of res) {
+    const randomIdx = Math.floor(Math.random() * JOB_TITLES.length);
+    await db
+      .update(users)
+      .set({ jobTitle: JOB_TITLES[randomIdx] })
+      .where(eq(users.id, user.id));
+  }
+  pool.end();
+}
+
+main();
