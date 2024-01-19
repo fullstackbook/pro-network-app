ce31253d171b1845558ae0f110fc87fc62a9ddaf
Travis Luong <travis@travisluong.com>
2024-01-19 20:22:25 +0800

    loading page for person detail

--- app/(dashboard)/dashboard/people/[id]/page.tsx
+++ app/(dashboard)/dashboard/people/[id]/page.tsx
@@ -1,6 +1,8 @@
 export default async function Page({ params }: { params: { id: string } }) {
   const { id } = params;
 
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
   return (
     <div>
       <p>{id}</p>
