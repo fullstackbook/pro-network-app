4017e77cea83556bd5382c8b98a6627a7d07e1a2
Travis Luong <travis@travisluong.com>
2024-01-19 12:56:58 +0800

    mobile burger

--- components/app-shell-container.tsx
+++ components/app-shell-container.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { AppShell } from "@mantine/core";
+import { AppShell, Burger } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
 
 export default function AppShellContainer({
@@ -8,9 +10,28 @@ export default function AppShellContainer({
 }: {
   children: ReactNode;
 }) {
+  const [opened, { toggle }] = useDisclosure();
+  const pathname = usePathname();
+
   return (
-    <AppShell>
-      <AppShell.Header>header</AppShell.Header>
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: "sm", collapsed: { mobile: !opened } }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <div className="flex justify-between h-full">
+          <div className="flex h-full items-center gap-5 p-5">
+            <Burger
+              opened={opened}
+              onClick={toggle}
+              hiddenFrom="sm"
+              size="sm"
+            />
+            Professional Network
+          </div>
+        </div>
+      </AppShell.Header>
       <AppShell.Navbar>navbar</AppShell.Navbar>
       <AppShell.Main>{children}</AppShell.Main>
     </AppShell>
