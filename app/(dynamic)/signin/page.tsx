0f8bbf6d2c8028bf3ddec722db9f021a6d6ef6c8
Travis Luong <travis@travisluong.com>
2024-01-19 12:20:31 +0800

    dashboard page stub

--- app/(dynamic)/signin/page.tsx
+++ app/(dynamic)/signin/page.tsx
@@ -8,7 +8,7 @@ export default async function SignIn() {
   const session = await getServerSession(authOptions);
 
   if (session) {
-    redirect("/");
+    redirect("/dashboard");
   }
 
   const providers: any = await getProviders();
