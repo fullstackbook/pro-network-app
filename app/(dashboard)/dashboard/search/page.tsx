ed80004eeda9e54ef76f3450905423b5b84faeb3
Travis Luong <travis@travisluong.com>
2024-01-21 14:50:08 +0800

    fuzzy search and raw sql

--- app/(dashboard)/dashboard/search/page.tsx
+++ app/(dashboard)/dashboard/search/page.tsx
@@ -1,7 +1,7 @@
 import UsersTable from "@/components/users-table";
 import { db } from "@/lib/db";
 import { users } from "@/lib/schema";
-import { ilike, or } from "drizzle-orm";
+import { ilike, or, sql } from "drizzle-orm";
 
 interface Props {
   searchParams: { query: string };
@@ -20,9 +20,25 @@ async function getUsers(query: string) {
   return res;
 }
 
+async function fuzzySearch(query: string) {
+  const res = await db
+    .select()
+    .from(users)
+    .where(
+      or(
+        sql`similarity(name, ${query}) > 0.2`,
+        sql`similarity(first_name, ${query}) > 0.2`,
+        sql`similarity(last_name, ${query}) > 0.2`,
+        sql`similarity(job_title, ${query}) > 0.2`
+      )
+    );
+  return res;
+}
+
 export default async function Page({ searchParams }: Props) {
   const { query } = searchParams;
-  const users = await getUsers(query);
+  // const users = await getUsers(query);
+  const users = await fuzzySearch(query);
 
   return (
     <div>
