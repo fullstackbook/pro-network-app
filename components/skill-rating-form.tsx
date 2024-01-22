272fa9e11e1e50f02e0ccf6f041c3f2f6ee38cc6
Travis Luong <travis@travisluong.com>
2024-01-22 12:34:56 +0800

    update skill rating action and revalidate path

--- components/skill-rating-form.tsx
+++ components/skill-rating-form.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { updateSkillRating } from "@/lib/actions";
 import { Rating } from "@mantine/core";
 import { useFormState } from "react-dom";
 
@@ -11,10 +12,15 @@ export function SkillRatingForm({
   skillId: string;
 }) {
   const initialState = {};
-  // const [state, dispatch] = useFormState()
+  const [state, dispatch] = useFormState(updateSkillRating, initialState);
 
   async function handleChange(newRating: number) {
     console.log(newRating);
+    const formData = new FormData();
+    formData.set("rating", newRating.toString());
+    formData.set("skillId", skillId);
+    await dispatch(formData);
+    console.log(state);
   }
 
   return <Rating value={rating} onChange={handleChange} />;
--- lib/actions.ts
+++ lib/actions.ts
@@ -4,8 +4,9 @@ import { z } from "zod";
 import { authOptions } from "./auth";
 import { getServerSession } from "next-auth";
 import { db } from "./db";
-import { users } from "./schema";
-import { eq } from "drizzle-orm";
+import { users, usersToSkills } from "./schema";
+import { and, eq } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
 
 const UpdateUserSchema = z.object({
   jobTitle: z.string().min(3),
@@ -51,5 +52,54 @@ export async function updateUser(
 
   return {
     success: "Update user success",
+  };
+}
+
+const UpdateSkillRatingSchema = z.object({
+  skillId: z.string().uuid(),
+  rating: z.number().min(1).max(5),
+});
+
+interface UpdateSkillRatingState {
+  errors?: {
+    skillId?: string[];
+    rating?: string[];
+  };
+  message?: string;
+  success?: string;
+}
+
+export async function updateSkillRating(
+  prevState: UpdateSkillRatingState,
+  formData: FormData
+): Promise<UpdateSkillRatingState> {
+  const session = await getServerSession(authOptions);
+
+  const validatedFields = UpdateSkillRatingSchema.safeParse({
+    skillId: formData.get("skillId"),
+    rating: parseInt(formData.get("rating")?.toString()!),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Update skill error",
+    };
+  }
+
+  await db
+    .update(usersToSkills)
+    .set({ rating: validatedFields.data.rating })
+    .where(
+      and(
+        eq(usersToSkills.skillId, validatedFields.data.skillId),
+        eq(usersToSkills.userId, session?.user.id)
+      )
+    );
+
+  revalidatePath("/dashboard/profile/skills");
+
+  return {
+    success: "Update skill success",
   };
 }
