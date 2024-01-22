840127bcf79c95dfaef58375aac4bfd93bd9e9e3
Travis Luong <travis@travisluong.com>
2024-01-22 16:01:12 +0800

    user card

--- app/(dashboard)/dashboard/people/[id]/page.tsx
+++ app/(dashboard)/dashboard/people/[id]/page.tsx
@@ -1,11 +1,31 @@
+import UserCard from "@/components/user-card";
+import { db } from "@/lib/db";
+import { users } from "@/lib/schema";
+import { eq } from "drizzle-orm";
+import { notFound } from "next/navigation";
+
+async function getUserById(id: string) {
+  return await db.query.users.findFirst({
+    where: eq(users.id, id),
+    with: { usersToUsersSkills: { with: { skill: true } } },
+  });
+}
+
 export default async function Page({ params }: { params: { id: string } }) {
   const { id } = params;
+  const user = await getUserById(id);
 
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+  if (!user) {
+    notFound();
+  }
+
+  // await new Promise((resolve) => setTimeout(resolve, 2000));
 
   return (
     <div>
-      <p>{id}</p>
+      <div>
+        <UserCard user={user} />
+      </div>
     </div>
   );
 }
