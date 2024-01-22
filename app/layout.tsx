df8f93fb5a3273705b00a2181ab316629a83714f
Travis Luong <travis@travisluong.com>
2024-01-22 15:31:03 +0800

    tailwindcss with mantine dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <head>
         <ColorSchemeScript />
       </head>
