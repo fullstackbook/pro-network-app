94b739687aff9b716ff52d1f5951eca9f4291a58
Travis Luong <travis@travisluong.com>
2024-01-22 13:10:32 +0800

    wire up the form to action

--- components/add-user-to-skill-btn.tsx
+++ components/add-user-to-skill-btn.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { AddUsersToSkillsState, addUsersToSkills } from "@/lib/actions";
 import { Skill } from "@/lib/types";
-import { Button, Modal, Select, Stack } from "@mantine/core";
+import { Button, Modal, Rating, Select, Stack } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { IconPlus } from "@tabler/icons-react";
+import { useState } from "react";
+import { useFormState } from "react-dom";
 
 export default function AddUserToSkillBtn({
   allSkills,
@@ -11,6 +14,11 @@ export default function AddUserToSkillBtn({
   allSkills: Skill[];
 }) {
   const [opened, { open, close }] = useDisclosure();
+  const [value, setValue] = useState(0);
+
+  const initialState: AddUsersToSkillsState = {};
+
+  const [state, dispatch] = useFormState(addUsersToSkills, initialState);
 
   const data = allSkills.map((skill) => {
     return {
@@ -21,9 +29,23 @@ export default function AddUserToSkillBtn({
   return (
     <>
       <Modal opened={opened} onClose={close} title="Add Skill" centered>
-        <form>
+        <form action={dispatch}>
           <Stack gap={20}>
-            <Select data={data} searchable name="skillId" />
+            <Select
+              data={data}
+              searchable
+              name="skillId"
+              error={state?.errors?.skillId}
+            />
+            <Rating value={value} onChange={setValue} name="rating" />
+            {state.errors?.rating && (
+              <p className="text-red-600">{state.errors.rating}</p>
+            )}
+            <div>
+              <Button type="submit">Submit</Button>
+            </div>
+            {state.message && <p className="text-red-600">{state.message}</p>}
+            {state.success && <p className="text-green-600">{state.success}</p>}
           </Stack>
         </form>
       </Modal>
