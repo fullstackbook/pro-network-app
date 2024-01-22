16700a337722683c5c8ef5b947cdb49a44c5f118
Travis Luong <travis@travisluong.com>
2024-01-22 11:16:05 +0800

    editor style

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
 import "@mantine/tiptap/styles.css";
+import "@/styles/editor.css";
 
 import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
