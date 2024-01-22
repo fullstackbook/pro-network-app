609e88ab6759e1261b336900d858d7d7e73b7d6c
Travis Luong <travis@travisluong.com>
2024-01-22 14:37:41 +0800

    mantine color scheme

---
+++ app/(dashboard)/dashboard/profile/color-scheme/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Button, Group, useMantineColorScheme } from "@mantine/core";
+
+export default function Page() {
+  const { setColorScheme, clearColorScheme } = useMantineColorScheme();
+
+  return (
+    <Group>
+      <Button onClick={() => setColorScheme("light")}>Light</Button>
+      <Button onClick={() => setColorScheme("dark")}>Dark</Button>
+      <Button onClick={() => setColorScheme("auto")}>Auto</Button>
+      <Button onClick={() => clearColorScheme}>Clear</Button>
+    </Group>
+  );
+}
