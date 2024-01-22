74d83fd4d5c865f707f1e14d6cca012ab8c4ea1b
Travis Luong <travis@travisluong.com>
2024-01-22 15:01:14 +0800

    color scheme and theme object

--- app/(dashboard)/dashboard/profile/color-scheme/page.tsx
+++ app/(dashboard)/dashboard/profile/color-scheme/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
       <Button onClick={() => setColorScheme("light")}>Light</Button>
       <Button onClick={() => setColorScheme("dark")}>Dark</Button>
       <Button onClick={() => setColorScheme("auto")}>Auto</Button>
-      <Button onClick={() => clearColorScheme}>Clear</Button>
+      <Button onClick={() => clearColorScheme()}>Clear</Button>
     </Group>
   );
 }
