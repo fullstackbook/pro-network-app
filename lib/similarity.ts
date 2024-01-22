936f77b9c6a16383c17c268c7bb7836260c72663
Travis Luong <travis@travisluong.com>
2024-01-22 18:04:46 +0800

    implement and test combined similarity

--- lib/similarity.ts
+++ lib/similarity.ts
@@ -17,3 +17,21 @@ export function cosineSimilarity(vector1: number[], vector2: number[]): number {
 
   return dotProduct / (magnitude1 * magnitude2);
 }
+
+// Function to calculate similarity based on skills and job title
+export function combinedSimilarity(
+  user1Skills: number[],
+  user2Skills: number[],
+  jobTitle1: string,
+  jobTitle2: string
+): number {
+  const skillsSimilarity = cosineSimilarity(user1Skills, user2Skills);
+
+  // additional criteria for job title
+  const jobTitleSimilarity = jobTitle1 === jobTitle2 ? 1 : 0;
+
+  // adjust weights
+  const combinedSimilarity = 0.8 * skillsSimilarity + 0.2 * jobTitleSimilarity;
+
+  return combinedSimilarity;
+}
--- tests/similarity.test.ts
+++ tests/similarity.test.ts
@@ -1,4 +1,4 @@
-import { cosineSimilarity } from "@/lib/similarity";
+import { combinedSimilarity, cosineSimilarity } from "@/lib/similarity";
 import { expect, test } from "vitest";
 
 test("cosine similarity exact vectors", () => {
@@ -34,4 +34,28 @@ test("cosine similarity some overlap", () => {
   const vector2 = [0, 0, 3, 4, 5];
   const score = cosineSimilarity(vector1, vector2);
   expect(score).toBe(0.47356801693808104);
+});
+
+test("combined similarity same job title", () => {
+  const vector1 = [5, 4, 3, 5, 0];
+  const vector2 = [0, 0, 3, 4, 5];
+  const score = combinedSimilarity(
+    vector1,
+    vector2,
+    "Full Stack Developer",
+    "Full Stack Developer"
+  );
+  expect(score).toBe(0.5788544135504649);
+});
+
+test("combined similarity different job title", () => {
+  const vector1 = [5, 4, 3, 5, 0];
+  const vector2 = [0, 0, 3, 4, 5];
+  const score = combinedSimilarity(
+    vector1,
+    vector2,
+    "Back End Developer",
+    "Front End Developer"
+  );
+  expect(score).toBe(0.37885441355046484);
 });
