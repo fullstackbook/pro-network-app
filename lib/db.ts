4b21a82e620f363e7b5f1220d1058c72e919f76e
Travis Luong <travis@travisluong.com>
2024-01-23 16:07:41 +0800

    add sslmode require to postgres url

--- lib/db.ts
+++ lib/db.ts
@@ -3,8 +3,13 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import * as schema from "./schema";
 
+let sslmode = "";
+if (config.APP_ENV === "prod") {
+  sslmode = "?sslmode=require";
+}
+
 export const pool = new Pool({
-  connectionString: config.POSTGRES_URL,
+  connectionString: config.POSTGRES_URL + sslmode,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
