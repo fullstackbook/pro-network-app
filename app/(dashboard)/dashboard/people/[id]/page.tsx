8ed4289507d962204f3801cb0a86ab060584361c
Travis Luong <travis@travisluong.com>
2024-01-23 12:58:43 +0800

    display similar people

--- app/(dashboard)/dashboard/people/[id]/page.tsx
+++ app/(dashboard)/dashboard/people/[id]/page.tsx
@@ -1,5 +1,6 @@
 import UserCard from "@/components/user-card";
 import { db } from "@/lib/db";
+import { getKNearestNeighborsByUserId } from "@/lib/knn";
 import { users } from "@/lib/schema";
 import { eq } from "drizzle-orm";
 import { notFound } from "next/navigation";
@@ -11,9 +12,15 @@ async function getUserById(id: string) {
   });
 }
 
+async function getSimilarPeople(userId: string) {
+  const similarPeople = await getKNearestNeighborsByUserId(userId, 5);
+  return similarPeople;
+}
+
 export default async function Page({ params }: { params: { id: string } }) {
   const { id } = params;
   const user = await getUserById(id);
+  const similarPeople = await getSimilarPeople(id);
 
   if (!user) {
     notFound();
@@ -32,6 +39,14 @@ export default async function Page({ params }: { params: { id: string } }) {
           className="prose dark:prose-invert"
           dangerouslySetInnerHTML={{ __html: user.bio ?? "" }}
         />
+        <div>
+          {similarPeople.map((person) => (
+            <div key={person.user.id}>
+              {person.user.firstName} {person.user.lastName}{" "}
+              {person.user.jobTitle} {person.similarity}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
