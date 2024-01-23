4a0906ec8f27da02e7a676482a65918d2ed768ff
Travis Luong <travis@travisluong.com>
2024-01-23 13:05:18 +0800

    style similar people

--- app/(dashboard)/dashboard/people/[id]/page.tsx
+++ app/(dashboard)/dashboard/people/[id]/page.tsx
@@ -2,6 +2,7 @@ import UserCard from "@/components/user-card";
 import { db } from "@/lib/db";
 import { getKNearestNeighborsByUserId } from "@/lib/knn";
 import { users } from "@/lib/schema";
+import { Avatar, NumberFormatter } from "@mantine/core";
 import { eq } from "drizzle-orm";
 import { notFound } from "next/navigation";
 
@@ -39,11 +40,25 @@ export default async function Page({ params }: { params: { id: string } }) {
           className="prose dark:prose-invert"
           dangerouslySetInnerHTML={{ __html: user.bio ?? "" }}
         />
-        <div>
+        <h2 className="font-bold text-xl">Similar People</h2>
+        <div className="flex flex-col gap-5">
           {similarPeople.map((person) => (
-            <div key={person.user.id}>
-              {person.user.firstName} {person.user.lastName}{" "}
-              {person.user.jobTitle} {person.similarity}
+            <div
+              key={person.user.id}
+              className="flex flex-row items-center gap-5"
+            >
+              <Avatar src={person.user.image} size="md" />
+              <div>
+                {person.user.firstName} {person.user.lastName}{" "}
+                {person.user.jobTitle}
+              </div>
+              <div className="flex-grow text-right">
+                <NumberFormatter
+                  value={person.similarity * 100}
+                  suffix="%"
+                  decimalScale={2}
+                />
+              </div>
             </div>
           ))}
         </div>
